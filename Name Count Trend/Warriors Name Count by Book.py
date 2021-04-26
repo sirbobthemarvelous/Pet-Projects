64f11ba1d2dfa9cd95ca020931ee6e6dc603b667
sirbobthemarvelous <63413318+sirbobthemarvelous@users.noreply.github.com>
2021-04-26 16:42:57 -0400

    Second Commit
    Lets do Machine Learning

---
+++ Name Count Trend/Warriors Name Count by Book.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as pyplot
+from matplotlib import style
+from joblib import dump, load
+
+#data = pd.read_csv(r"C:\Users\robzh\Documents\python\Pet Projects Only For Fun (not Counting Nifty)\Name Count Trend\fire-count.csv", sep=';')
+
+print(data.head())
