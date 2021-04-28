4a82f3f6016a100c7e1fee94e3bd41171fd7436e
sirbobthemarvelous <63413318+sirbobthemarvelous@users.noreply.github.com>
2021-04-28 11:08:48 -0400

    Functional Build aka v2
    LInear Regression was always going to be inaccurate I just want to be able to implement it

--- Name Count Trend/Warriors Name Count by Book.py
+++ Name Count Trend/Warriors Name Count by Book.py
@@ -8,4 +8,28 @@ from joblib import dump, load
 
 data = pd.read_csv(r"C:\Users\robzh\Documents\python\Pet Projects Only For Fun (not Counting Nifty)\Name Count Trend\FirestarNamedrops.csv")
 
-print(data.head())
+X = data['Main Book'].values.reshape(-1,1)
+# you have to use array.reshape(-1,1) if its a single feature when using the predict function now
+y = data['Firestars']
+#print(data.head())
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=5 ,random_state=1)
+#test size is specifically 5 rather than a percentage, random stat is none by default tho
+linear = LinearRegression()
+linear.fit(X_train,y_train) # creates the LIne of best fit....eqn
+
+y_pred = linear.predict(X_test)
+#print(y_pred)
+
+style.use("ggplot")
+# Drawing and plotting model
+#pyplot.plot(x_test, predictions, label = "Linear Regression", color = 'b')
+pyplot.scatter(X_test, y_test, color='red') 
+pyplot.plot(X_test, y_pred, color = 'blue')
+pyplot.title('Number of Times Firestar is Namedropped by Book')
+pyplot.xlabel('Mainline Book Number')
+pyplot.ylabel("Number of Firestars")
+pyplot.show()
+
+#model scoring
+print(linear.score(X_test,y_test))
