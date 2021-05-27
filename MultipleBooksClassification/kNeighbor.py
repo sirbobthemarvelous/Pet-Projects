6728bc16bf274be1c6733d77cd0b7cf1a7a58ffa
sirbobthemarvelous <63413318+sirbobthemarvelous@users.noreply.github.com>
2021-05-27 11:45:05 -0400

    just moving things around
    i like taking away notifs

--- MultipleBooksClassification/kNeighbor.py
+++ MultipleBooksClassification/kNeighbor.py
@@ -5,23 +5,25 @@ import pandas as pd
 import numpy as np
 from sklearn import linear_model, preprocessing
 
-data = pd.read_csv("car.data")
+data = pd.read_csv(r"C:\Users\robzh\Documents\python\Pet Projects Only For Fun (not Counting Nifty)\MultipleBooksClassification\Arc 1 Everyone.csv")
 print(data.head())
 
-optimusPrime = preprocessing.LabelEncoder()
-buying = optimusPrime.fit_transform(list(data["buying"]))
-maint = optimusPrime.fit_transform(list(data["maint"]))
-doors = optimusPrime.fit_transform(list(data["doors"]))
-persons = optimusPrime.fit_transform(list(data["persons"]))
-lug_boot = optimusPrime.fit_transform(list(data["lug_boot"]))
-safety = optimusPrime.fit_transform(list(data["safety"]))
-assClass = optimusPrime.fit_transform(list(data["class"]))
+# preprocessing.LabelEncoder() and then .fit_transform is good for when the data isn't on the same scale
+
+name = (list(data["Name"]))
+book1 = (list(data["Firestar"]))
+book2 = (list(data["Graystripe"]))
+book3 = (list(data["Bluestar"]))
+book4 = (list(data["Tree"]))
+book5 = (list(data["Sandstorm"]))
+book6 = (list(data["Cloudtail"]))
+booktotal= (list(data['Tallstar']))
 # turn low medium high into 1,2,3, turning it into numpy arrays to transform them
 
 predict = "class"
 
-x = list(zip(buying, maint, doors, persons, lug_boot, safety))
-y = list(assClass)
+x = list(zip(book1, book2, book3, book4, book5, book6))
+y = list(booktotal)
 # zip creates tuple objects...the numpy array
 
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
