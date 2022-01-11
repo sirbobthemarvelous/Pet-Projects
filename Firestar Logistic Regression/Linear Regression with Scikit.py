bb83d2d1cecab1c140320efbd9e246fe4ff58e7d
sirbobthemarvelous <63413318+sirbobthemarvelous@users.noreply.github.com>
2022-01-11 14:51:17 -0500

    small edits

---
+++ Firestar Logistic Regression/Linear Regression with Scikit.py
@@ -0,0 +1,70 @@
+#ok no this dataset is a bit too complicated just jump into warrior cats
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+#use scikit-learn over sklearn
+#https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LinearRegression.html
+import matplotlib.pyplot as pyplot
+from matplotlib import style
+from joblib import dump, load
+#alternate version of pickle https://scikit-learn.org/stable/modules/model_persistence.html
+
+
+data = pd.read_csv(r"C:\Users\robzh\Documents\python\Pet Projects Only For Fun (not Counting Nifty)\Name Count Trend\student-mat.csv.csv", sep=';')
+
+print(data.head())
+
+data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
+
+predict = "G3"
+
+x = np.array(data.drop([predict], 1))
+y = np.array(data[predict])
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1)
+# https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
+ # splitting the test samples as 10% of the data so they don't cheat by looking at the end
+
+best = 0
+for _ in range(300):
+
+    linear = LinearRegression()
+
+    linear.fit(x_train, y_train)
+    # fits the data to find a best fit line
+    accuracy = linear.score(x_test, y_test)
+    print(accuracy)
+
+'''
+    if accuracy > best:
+        best = accuracy
+        
+        with open("studentmodel.pickle", "wb") as f:
+            pickle.dump(linear, f)
+            # save a pickle file in a directory """
+
+
+pickle_in = open("studentmodel.pickle", "rb")
+linear = pickle.load(pickle_in)
+
+print('Co: \n', + linear.coef_)
+print('Intercept: \n', + linear.intercept_)
+'''
+
+#above is the scoring system and the pickle
+
+predictions = linear.predict(x_test)
+#takes two dimentional arrays and makea bunch of predictions
+for x in range(len(predictions)):
+    print(predictions[x], x_test[x], y_test[x])
+
+style.use("ggplot")
+# Drawing and plotting model
+plot = "G1" # Change this to G1, G2, studytime or absences to see other graphs
+pyplot.plot(x_test, predictions, label = "Linear Regression", color = 'b')
+pyplot.scatter(data[plot], data["G3"])
+pyplot.legend(loc=4)
+pyplot.xlabel(plot)
+pyplot.ylabel("Final Grade")
+pyplot.show()
---
+++ Firestar Logistic Regression/Warriors Name Count by Book 2.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as pyplot
+from matplotlib import style
+from joblib import dump, load
+
+data = pd.read_csv(r"C:\Users\robzh\Documents\python\Pet Projects Only For Fun (not Counting Nifty)\Name Count Trend\FirestarNamedrops.csv")
+
+X = data['Main Book'].values.reshape(-1,1)
+# you have to use array.reshape(-1,1) if its a single feature when using the predict function now
+y = data['Firestars']
+#print(data.head())
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2 ,random_state=1)
+#test size is specifically 5 rather than a percentage, random stat is none by default tho
+#actually it looks like even at 0 there's randomization in the test training split so, just go with percentage
+
+linear = LinearRegression()
+linear.fit(X_train,y_train) # creates the LIne of best fit....eqn
+
+y_pred = linear.predict(X_test)
+#print(y_pred)
+
+style.use("ggplot")
+# Drawing and plotting model
+#The ACTUAL TRAINING SET and Line of Best Fit
+pyplot.scatter(X_train, y_train, color='red') 
+pyplot.plot(X_test, y_pred, color = 'blue')
+pyplot.title('Number of Times Firestar is Namedropped by Book')
+pyplot.xlabel('Mainline Book Number')
+pyplot.ylabel("Number of Firestars")
+pyplot.show()
+
+# The ACTUAL TEST SET and Line of Best Fit
+#pyplot.plot(x_test, predictions, label = "Linear Regression", color = 'b')
+pyplot.scatter(X_test, y_test, color='red') 
+pyplot.plot(X_test, y_pred, color = 'blue')
+pyplot.title('Number of Times Firestar is Namedropped by Book')
+pyplot.xlabel('Mainline Book Number')
+pyplot.ylabel("Number of Firestars")
+pyplot.show()
+
+#model scoring
+print(linear.score(X_test,y_test))
